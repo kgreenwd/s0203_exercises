2a9598a8fae56cfbc237240682f91e11da951efd
kgreenwd <2013kgreenwood@gmail.com>
2015-07-14 15:23:23 +0200

    completed exercise 1, full-name-greeting.rb

---
+++ KleoGreenwood-kgreenwood/d2/full-name-greeting.rb
@@ -0,0 +1,7 @@
+puts "Oh, hey.  What's your first name again?"
+firstname = gets.strip.capitalize
+puts "Oh, right right... what's your middle name?  Asking for a friend."
+middlename = gets.strip.capitalize
+puts "Right, yeah I knew that.  Uh... last name?"
+lastname = gets.strip.capitalize
+puts "Haha yeah I was just, uh, messin' with you!  Hi there #{firstname} #{middlename} #{lastname}!} "
