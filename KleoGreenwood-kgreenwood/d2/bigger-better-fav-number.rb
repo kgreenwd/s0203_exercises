3f3c9200a1efbe76e24ddf89600875422fd0ba62
kgreenwd <2013kgreenwood@gmail.com>
2015-07-14 15:32:45 +0200

    completed exercise 2, bigger-better-favorite-number.rb

---
+++ KleoGreenwood-kgreenwood/d2/bigger-better-fav-number.rb
@@ -0,0 +1,4 @@
+puts "Yo dude!  What's your favorite number?"
+puts favnumber = gets.strip.to_i
+newfavnumber = favnumber + 1
+puts "Oh uh... yeah that sure is a... great favorite number!  Um, have you ever thought about maybe... I dunno... switching to #{newfavnumber}?  Not a sermon, just a thought."
