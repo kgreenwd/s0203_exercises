735dc371c433070142129a49fd1c95bf23fba2dc
kgreenwd <2013kgreenwood@gmail.com>
2015-07-16 16:10:44 +0200

    completed 3B exercise 4, hashing it out

---
+++ KleoGreenwood-kgreenwood/d4/recipes.rb
@@ -0,0 +1,19 @@
+recipes = {
+	:hash_browns => {
+		:description => "Crunchy, buttery, delicious, potatoe-based heaven.",
+		:ingredients => ["potatoes", "butter", "egg", "flour"],
+		:steps => ["Shred potatoes", "Mix in onion, flour, and egg", "Heat oil and place potatoes into the pan", "Cook until browned then flip over and brown the other side", "Remove from pan"]
+	},
+	:spaghetti_and_meatballs => {
+		:description => "Carbs and tomatoes and meat, yum!",
+		:ingredients => ["potatoes", "butter", "egg", "flour"],
+		:steps => ["Bring water to boil", "Add pasta and stir occasionally", "Strain pasta", "Pat beef into balls", "Brown meatballs in skillet", "Heat tomato sauce"]
+	},
+	:donuts => {
+		:description => "Glazed, cakey goodness.  See also: 'crack'",
+		:ingredients => ["pasta", "tomato sauce", "ground beef"],
+		:steps => ["Beat together sugar, flour, egg, and melted butter", "Cover with plastic wrap and chill for one hour", "Heat an inch of oil to 360 degrees F", "Roll dough out and cut out center hole", "Drop donuts into the hot oil, turning occasionally", "Remove after about 3 minutes"]
+	}
+}
+
+#puts recipes[:hash_browns][:ingredients][3]
